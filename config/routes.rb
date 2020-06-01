3731a7087d605ed8ec4f9d2b78eabec4aac60426
Alexandru Bangau <rammazzoti2000@gamil.com>
2020-06-01 17:20:19 +0200

    Create a User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/new'
   get 'static_pages/home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'application#hello'
+  get  '/signup',  to: 'users#new'
 end
