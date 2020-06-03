f4f696bfc26b74bd6874728d59dfc82c266500e5
Alexandru Bangau <rammazzoti2000@gamil.com>
2020-06-03 22:47:01 +0200

    bug on aws secrete key caused by fog gem

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -4,7 +4,11 @@ class AvatarUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  if Rails.env.development? || Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
--- app/uploaders/featured_image_uploader.rb
+++ app/uploaders/featured_image_uploader.rb
@@ -4,7 +4,13 @@ class FeaturedImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  if Rails.env.development? || Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
+
+  # storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
