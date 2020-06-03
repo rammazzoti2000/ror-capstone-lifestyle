bd1e3a0e895f84e254b5e03209c9e8128ff23cff
Alexandru Bangau <rammazzoti2000@gamil.com>
2020-06-03 12:41:56 +0200

    now you create category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,8 +6,26 @@ class CategoriesController < ApplicationController
   end
 
   def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      flash['alert-success'] = 'Category created'
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def show
   end
+
+  private
+
+    def category_params
+      params.require(:category).permit(:name, :priority)
+    end
 end
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,10 +1,18 @@
 module ArticlesHelper
   def string_length(string, length)
-   s = string.length > length ? "#{string[0..length]}..." : string
-   s
+    string_sanitize = ActionView::Base.full_sanitizer.sanitize(string)
+    string_sanitize.length > length ? "#{string_sanitize[0..length]}..." : string_sanitize
   end
 
-  def get_order(num)
+  def get_order_1(num)
+    num.odd? ? 2 : 1
+  end
+
+  def get_order_2(num)
+    num.odd? ? 1 : 2
+  end
+
+  def parent_order_lg(num)
     if ((num - 1) % 4).zero?
       num + 1
     elsif ((num - 2) % 4).zero?
@@ -13,6 +21,18 @@ module ArticlesHelper
       num
     elsif ((num - 4) % 4).zero?
       num
+    end
+  end
+
+  def parent_order_sm(num)
+    if ((num - 1) % 4).zero?
+      1
+    elsif ((num - 2) % 4).zero?
+      3
+    elsif ((num - 3) % 4).zero?
+      2
+    elsif ((num - 4) % 4).zero?
+      4
     end
   end
 
@@ -25,6 +45,22 @@ module ArticlesHelper
   end
 
   def min_read(article)
-    (article.length / 100).to_i
+    article_sanitize = ActionView::Base.full_sanitizer.sanitize(article)
+    (article_sanitize.length / 100).to_i
+  end
+
+  def search_info(search, searched_count)
+    searched_info = ''
+    if searched_count.present?
+      searched_info = "Search for \"#{search}\"<br> "
+      searched_info += "Result for #{pluralize(searched_count.count, 'user')}"
+    else
+      if search.present?
+        searched_info = "No search for \"#{search}\""
+      else
+        searched_info = 'Search for something'
+      end
+    end
+    searched_info.html_safe
   end
 end
