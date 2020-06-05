343f52ddd56425d53c92c0334a72766e40268058
Alexandru Bangau <rammazzoti2000@gamil.com>
2020-06-05 23:53:29 +0200

    more heroku deploy fixes

--- app/uploaders/featured_image_uploader.rb
+++ app/uploaders/featured_image_uploader.rb
@@ -4,14 +4,14 @@ class FeaturedImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  # if Rails.env.development? || Rails.env.test?
-  #   storage :file
-  # else
-  #   storage :fog
-  # end
+  if Rails.env.development? || Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
 
   # storage :file
-  storage :fog
+  # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,10 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV['S3_KEY'],
-    aws_secret_access_key: ENV['S3_SECRET'],
+    # aws_access_key_id: ENV['S3_KEY'],
+    # aws_secret_access_key: ENV['S3_SECRET'],
+    aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
     region: ENV['S3_REGION']
   }
   config.fog_directory  = ENV['S3_BUCKET']
