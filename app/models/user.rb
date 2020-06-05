df87885aa3ad4c1597686e4d14040b688a0bd667
Alexandru Bangau <rammazzoti2000@gamil.com>
2020-06-06 01:17:58 +0200

    fix authenticate method, misspelled

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,7 +40,7 @@ class User < ApplicationRecord
   end
 
   # Returns true if the given token matches the digest.
-  def authenticated?(remember_token)
+  def authenticate?(remember_token)
     return false if remember_digest.nil?
 
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
